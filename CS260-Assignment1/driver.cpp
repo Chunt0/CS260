f54d43ef4fc7a520586288bf4e208dd6d2391069
Chunt0 <chunt.mail@gmail.com>
2022-04-10 09:33:42 -0700

    Commit

--- CS260-Assignment1/driver.cpp
+++
@@ -1,7 +0,0 @@
-#include "bag.h"
-
-int main(){
-    Bag my_bag;
-    my_bag.selectionLoop();
-    return 0;
-}
--- CS260-Assignment2/README.md
+++
@@ -1,3 +0,0 @@
-# CS260-Assignment2
-
-This repo contains all materials related to Assignment 2
