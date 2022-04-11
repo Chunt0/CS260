0b8a0cdd63e8380e64f38dcf3a1fdc1f3497028a
Chunt0 <chunt.mail@gmail.com>
2022-04-11 08:10:20 -0700

    Updated 2022-04-11

---
+++ Assignment3/Queue/queue.h
@@ -0,0 +1,23 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <iostream>
+#include "node.h"
+
+class Queue{
+    
+    private:  
+    // Attributes
+    Node* head;
+    int position;
+
+    // Constructor/Destructor
+    Queue();
+    ~Queue();
+
+    // Methods
+    void enqueue();
+    void dequeue();
+};
+
+#endif
