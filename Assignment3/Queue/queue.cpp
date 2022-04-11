c607e38cd6d6f278ca40fa774bf75fa2427038fd
Chunt0 <chunt.mail@gmail.com>
2022-04-11 08:21:25 -0700

    Updated 2022-04-11

--- Assignment3/Queue/queue.cpp
+++ Assignment3/Queue/queue.cpp
@@ -6,18 +6,38 @@ Queue::Queue(){
 }
 
 Queue::~Queue(){
-
+    Node* current;
+    while(head != nullptr){
+        current = head;
+        head = head->next;
+        delete current;
+    }
 }
 
 void Queue::enqueue(){
     Node* n = new Node;
+    Node* current = head;
     position++;
     n->value = position;
-
-
-
+    if(head != nullptr){
+        while(current->next != nullptr){
+            current = current->next;
+        }
+        current->next = n;
+    }
+    else{
+        head = n;
+    }
 }
 
 void Queue::dequeue(){
-
+    Node* deletePtr;
+    if(head != nullptr){
+        deletePtr = head;
+        head = head->next;
+        delete deletePtr;
+    }
+    else{
+        std::cout << "The queue is empty." << std::endl;
+    }
 }
