fa9c68d5c5dcd7d0f3161e115d9f21781047dbc8
Chunt0 <chunt.mail@gmail.com>
2022-05-02 09:41:38 -0700

    Gitterdone

---
+++ Assignment6/wk6class1.cpp
@@ -0,0 +1,13 @@
+/* Christopher Hunt
+ * CS260
+ * Binary Trees
+ */
+
+#include <iostream>
+
+int main(int argc, char** argv){
+    std::cout << "argc: " << argc << std::endl;
+    std::cout << "argv[1]: " << argv[1] << std::endl;
+    return 0;
+
+}
